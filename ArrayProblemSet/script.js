91a75aed5cd795d6c858ec0ae56e7653c651c367
Mozzey <mbcacciano@gmail.com>
2017-12-30 14:53:00 -0500

    fixes to array problem set

--- ArrayProblemSet/script.js
+++ ArrayProblemSet/script.js
@@ -1,15 +1,17 @@
 var testArray = [1,2,3,4,5,10,20,30,40,55,100];
 
+//*** printReverse ***
 function printReverse(arr) {
 	for (var i = arr.length-1; i >= 0; i--) {
 		console.log(arr[i]);
 	}
 }
 
+//*** isUniform ***
 function isUniform(arr) {
 	var first = arr[0];
 
-	for(var i = 0; i < arr.length; i++) {
+	for(var i = 1; i < arr.length; i++) {
 		if (first !== arr[i]) {
 			return false;
 		}
@@ -17,10 +19,27 @@ function isUniform(arr) {
 	return true;
 }
 
+//*** sumOfAll ***
 function sumOfAll(arr) {
-	var count = 0;
+	var currentSum = 0;
 	arr.forEach(function(number) {
-		count += number;
+		currentSum += number;
 	})
-	return count;
+	return currentSum;
+}
+
+//*** max ***
+function max(arr) {
+	var firstNum = arr[0];
+	// for(var i = 1; i < arr.length; i++) {
+	// 	if (arr[i] > firstNum) {
+	// 		firstNum = arr [i];
+	// 	}
+	// }
+	arr.forEach(function(currentNum) {
+		if (currentNum >= firstNum) {
+			firstNum = currentNum;
+		}
+	})
+	return firstNum;
 }
