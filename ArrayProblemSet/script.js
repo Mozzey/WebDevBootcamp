a0431db8ba41423723da69d72c18555d6d5466ba
Michael Cacciano <MBCacciano@gmail.com>
2018-01-01 00:14:50 -0500

    random update

--- ArrayProblemSet/script.js
+++ ArrayProblemSet/script.js
@@ -1,45 +1,31 @@
 var testArray = [1,2,3,4,5,10,20,30,40,55,100];
 
-//*** printReverse ***
 function printReverse(arr) {
 	for (var i = arr.length-1; i >= 0; i--) {
 		console.log(arr[i]);
 	}
 }
 
-//*** isUniform ***
 function isUniform(arr) {
 	var first = arr[0];
-
-	for(var i = 1; i < arr.length; i++) {
-		if (first !== arr[i]) {
+	for(var i = 0; i < arr.length; i++) {
+		if (arr[i] !=== first) {
 			return false;
 		}
 	}
 	return true;
 }
 
-//*** sumOfAll ***
-function sumOfAll(arr) {
-	var currentSum = 0;
-	arr.forEach(function(number) {
-		currentSum += number;
-	})
-	return currentSum;
+function sumArray(arr) {
+	var counter = 0;
+	for(var i = 0; i < arr.length; i++) {
+		counter += arr[i]
+	}
+	return counter;
 }
 
-//*** max ***
-function max(arr) {
-	var firstNum = arr[0];
-	// for(var i = 1; i < arr.length; i++) {
-	// 	if (arr[i] > firstNum) {
-	// 		firstNum = arr [i];
-	// 	}
-	// }
-	arr.forEach(function(currentNum) {
-		if (currentNum >= firstNum) {
-			firstNum = currentNum;
-		}
-	})
-	return firstNum;
+function max() {
+	for(var i = 0; i < arr.length; i++) {
+		var counter = arr[i];
+	}
 }
---
+++ SelectorExcercise/script.js
@@ -0,0 +1,4 @@
+var one = document.getElementById("first");
+var two = document.querySelector(".special");
+var three = document.querySelector("p");
+var four = document.querySelector("#first");
